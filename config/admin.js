6413feb3cfdcb6ac1e2f91ed0994246dfa5a95d2
geraldsamosir <gerald.unforgiven@gmail.com>
2023-01-29 11:04:21 +0700

    chore(favicon): custom favicon admin

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
+  url: '/dashboard',
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
-  },
-
+  }
 });
